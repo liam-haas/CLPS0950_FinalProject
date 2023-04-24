977dc62b56ff4bafb704ef611e3ce804ed5f1267
liam-haas <86485039+liam-haas@users.noreply.github.com>
2023-04-24 12:34:27 -0400

    Update print('Hello').py

--- print('Hello').py
+++ print('Hello').py
@@ -1,3 +1,13 @@
 print('Hello')
 
 print('Hi: {}'.format(8))
+
+import rdkit
+from rdkit import Chem
+from rdkit.Chem import *
+from rdkit.Chem import Draw
+from rdkit.Chem.Draw import IPythonConsole
+from rdkit.Chem import Descriptors
+from rdkit.Chem import AllChem
+from rdkit import DataStructs
+import numpy as np
