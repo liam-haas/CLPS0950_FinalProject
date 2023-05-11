9d114d2e43236817d3303048f36e44714e0479bc
liam-haas <86485039+liam-haas@users.noreply.github.com>
2023-05-10 20:10:18 -0400

    Playing with tkinter

--- Tkinter.py
+++ Tkinter.py
@@ -1,6 +1,15 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
+from PIL import ImageTk, Image
 
-main = Tk()
+class ChemApp:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry('500x500')
+        self.root.title('Alkene Hydrobromination Predictor')
+        self.root.configure(bg = '#ADD8E6')
+        self.label = tk.Label(self.root, text = 'Organic Chemistry Helper!',
+                              font = ('Serif', 25), bg = '#ADD8E6')
+        self.label.pack(pady = 10)
+        self.root.mainloop()
 
-main.mainloop()
+ChemApp()
